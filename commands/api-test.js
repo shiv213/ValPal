5e037eaeba034786a4468d0fdb375b17e6afc409
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2020-07-22 19:22:40 -0400

    added role command
    Signed-off-by: Shiv Trivedi <shiv.v.trivedi@gmail.com>

--- commands/api-test.js
+++ commands/api-test.js
@@ -3,26 +3,13 @@ const request = require('request');
 module.exports = {
     name: 'api-test',
     description: 'Checks to see if the connection to the Riot Games API is working.',
-    aliases: ['api-check', 'apitest'],
+    aliases: ['api-check', 'apicheck', 'apitest'],
     cooldown: 30,
     execute(message, args) {
         request(`https://na.api.riotgames.com/val/content/v1/contents?locale=en-US&api_key=${val_apikey}`, { json: true }, (err, res, body) => {
             if (err) { return message.channel.send(err); }
             // console.log(body);
             message.channel.send('```json\n' + JSON.stringify(body.characters) + '\n```')
-
         });
-        // https.get(`https://na.api.riotgames.com/val/content/v1/contents?locale=en-US&api_key=${val_apikey}`, (resp) => {
-        //     let data = '';
-        //     resp.on('data', (chunk) => {
-        //         data += chunk;
-        //     });
-        //     resp.on('end', () => {
-        //         message.channel.send('```json\n' + JSON.stringify(JSON.parse(data).version) + '\n```')
-        //     });
-        //
-        // }).on("error", (err) => {
-        //     message.channel.send("Error: " + err.message);
-        // });
     },
 };
---
+++ commands/role.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'role',
+    description: 'Assigns roles to user',
+    args: true,
+    cooldown: 10,
+    execute(message, args) {
+        let role = message.guild.roles.cache.find(role => role.name === args[0]);
+        if (role) {
+            message.member.roles.add(role);
+            message.reply(`The **${role.name}** role has been given to you.`)
+        }
+
+    },
+};
+
+// // Krish's c0de
+// module.exports = {
+//     name: 'give-role',
+//     description: 'gives roles',
+//     execute(message, args) {
+//         var role = message.author.guild.roles.find ("name", args);
+//         if (args[0] === role)
+//             message.author.addRole(role)
+//
+//     },
+// };
